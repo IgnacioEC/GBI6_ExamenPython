3d0d16a602dfd6df6be209aaab7e2992f03cb4aa
IgnacioEC <105082641+IgnacioEC@users.noreply.github.com>
2022-08-05 16:50:17 -0500

    .

--- miningscience.py
+++ miningscience.py
@@ -1,34 +1,31 @@
-# NOMBRE (apellido, nombre): 
+# NOMBRE (Carranco, Ignacio): 
 
-from Bio import Entrez
+def download_pubmed(HPV18I1): 
+    """
+    Se hace un llamado de librería data de pubmed importando desde biopython para el gen HPV18I1 
+    perteneciente al Herpex virus en humanos
+    """
+    from Bio import Entrez
 
 Entrez.email="ignacio.carranco@est.ikiam.edu.ec"
 
 handle=Entrez.einfo(db="genome")
-record=Entrez.read(handle)
-
 handle=Entrez.esearch(db="pubmed", term = "cancer hpv")
 record=Entrez.read(handle)
+handle.close()
+print(rec.keys())
 print(record)
-
-
-
-def download_pubmed(HPVCANCER): 
-    """
-    Se hace un llamado de librería data de pubmed importando desde biopython
-    """
-    
     
     return 
 
 
 
-def mapscience(HPVCANCER): # DEBE IR EN DATA
+def mapscience(HPV18I1): # DEBE IR EN DATA
     """
     Definimos una función la cual llamaremos más adelante
  
     """
-    PUB1=PUB.txt
+     print('El número artículos para HPV18I1 es:') # Que se cargue con inserción de texto o valor que correspondea KEYWORD y XX
     
     
     
