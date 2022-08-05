ff603ec8c975e630613cce3b81dcb107e7e27ee8
IgnacioEC <105082641+IgnacioEC@users.noreply.github.com>
2022-08-05 14:20:26 -0500

    actualización

---
+++ miningscience.py
@@ -0,0 +1,58 @@
+# NOMBRE (apellido, nombre): 
+
+def download_pubmed(                ): 
+    """
+    Docstring download_pubmed
+    
+    
+    
+    
+    
+    """
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    return 
+
+
+
+def mapscience(                   ):
+    """
+    Docstring map_science
+    
+    
+    
+    
+    
+    
+    
+    """
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    return 
+
+    
