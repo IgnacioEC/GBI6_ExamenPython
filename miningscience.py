661a1e9d28e15f2d550224e4fd029f7ade1ca1b4
IgnacioEC <ignacio.carranco@est.ikiam.edu.ec>
2022-08-06 15:01:44 -0500

    .

--- miningscience.py
+++ miningscience.py
@@ -4,13 +4,13 @@ import Bio
 from Bio.Seq import Seq
 from Bio import Entrez
 import re
-
 def download_pubmed (VIRUELA):
     """
     Funcion que pide como input la palabra de busqueda en tipo str del pubmed y como output guarda un documento con extensión
     txt que contiene los datos de la busqueda y se hace un llamado de librería data de pubmed importando desde biopython para 
     el gen HPV18I1 (CANCERHPV) perteneciente al Herpex virus en humanos:
     El code `efetch` Recupera registros en el formato solicitado de una lista
+    
     """ 
 
     Entrez.email = "ignacio.carranco@est.ikiam.edu.ec"
@@ -18,8 +18,6 @@ def download_pubmed (VIRUELA):
                         term="VIRUELA",
                         usehistory="y")
     record = Entrez.read(handle)
-
-    
     id_list = record["IdList"]
     webenv = record["WebEnv"]
     query_key = record["QueryKey"]
@@ -30,17 +28,18 @@ def download_pubmed (VIRUELA):
                        retmax=543, 
                        webenv=webenv,
                        query_key=query_key)
-
-    out_handle = open("VIRUELA_pub.txt", "w")
+    out_handle = open("data/VIRUELA_pub.txt", "w")
     data = handle.read()
+    (id_list)
     handle.close()
     out_handle.write(data)
     out_handle.close()
+   
     
     return id_list
 
 
-def science_plots(VIRUELA): 
+def science_plots(PAISES, VIRUELA): 
     """
     Definimos una función con la cual llamaremos los datos de la pregunta 1 yse guardará 
     como archivo de extensión .txt
@@ -54,17 +53,17 @@ def science_plots(VIRUELA):
     
     with open("data/VIRUELA_pub.txt", errors="ignore") as l: 
         texto = l.read()
-    texto = re.sub(r"\n\s{6}", " ", texto)
-    countries_1 = re.findall (r"AD\s{2}-\s[A-Za-z].*,\s([A-Za-z]*)\.\s", texto)
-    unique_countries = list(set(countries_1))
-    conteo=Counter(countries_1)
-    resultado={} 
+        texto = re.sub(r"\n\s{6}", " ", texto)
+        countries_1 = re.findall (r"AD\s{2}-\s[A-Za-z].*,\s([A-Za-z]*)\.\s", texto)
+        unique_countries = list(set(countries_1))
+        conteo=Counter(countries_1)
+        resultado={} 
     for clave in conteo:  
         valor=conteo[clave]
         if valor > 1:
             resultado[clave] = valor
-    ordenar = (sorted(resultado.values()))
-    ordenar.sort(reverse=True) 
+            ordenar = (sorted(resultado.values()))
+            ordenar.sort(reverse=True) 
     import operator
     pais = [] 
     contador = []
@@ -78,7 +77,7 @@ def science_plots(VIRUELA):
     plt.pie(frecuencia_cinco, labels = cinco_paises)
     (plt.savefig("img/VIRUELA.jpg", dpi=100, bbox_inches='tight'))
     plt.show()
-    return (count_pais)
+    return (conteo)
     
     
     
